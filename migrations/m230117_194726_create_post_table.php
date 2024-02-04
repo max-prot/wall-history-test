750c6a21fe4c77a5c97273277c54f1c960adaffe
max-prot <max-prot@users.noreply.github.com>
2024-02-04 14:10:29 +0200

    fix: Исправление отображения кириллицы

--- migrations/m230117_194726_create_post_table.php
+++ migrations/m230117_194726_create_post_table.php
@@ -12,13 +12,18 @@ class m230117_194726_create_post_table extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('{{%post}}', [
             'id' => $this->primaryKey(),
             'author' => $this->string(55)->notNull(),
             'message' => $this->text(),
             'ip' => $this->string(128),
             'created_at' => $this->integer(11),
-        ]);
+        ], $tableOptions);
     }
 
     /**
